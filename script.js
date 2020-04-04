76079717f3b9d76dc3cb0aab7ab002a9e6ac0e85
mohamedisakr <mohamedisakr@gmail.com>
2020-04-04 22:09:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// https://www.youtube.com/watch?v=fHPa5xzbpaA
+
+function getUsers() {
+  return getFetch("https://jsonplaceholder.typicode.com/users");
+  //   return fetch("https://jsonplaceholder.typicode.com/users", {
+  //     method: "GET",
+  //     headers: { "Content-Type": "application/json" }
+  //   }).then(res => res.json());
+}
+
+function getUserPosts(userId) {
+  return getFetch("https://jsonplaceholder.typicode.com/posts", { userId });
+  //   return fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, {
+  //     method: "GET",
+  //     headers: { "Content-Type": "application/json" }
+  //   }).then(res => res.json());
+}
+
+getUsers().then(users => {
+  users.forEach(user => {
+    getUserPosts(user.id).then(posts => {
+      console.log(`Username : ${user.name}, No. of posts ${posts.length}`);
+    });
+  });
+});
+
+function getFetch(url, params = {}) {
+  const queryString = Object.entries(params)
+    .map(param => `${param[0]}=${param[1]}`)
+    .join("&");
+  return fetch(`${url}?${queryString}`).then(res => res.json());
+}
